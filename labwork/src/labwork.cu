5ca916ec67fa6da8a5ef50bf1d2c28f2512c9b39
luonghungson <luonghungewsxz@mail.com>
2017-11-21 20:59:22 +0700

    update labwork.cu

--- labwork/src/labwork.cu
+++ labwork/src/labwork.cu
@@ -131,8 +131,26 @@ int getSPcores(cudaDeviceProp devProp) {
 }
 
 void Labwork::labwork2_GPU() {
-    
-}
+
+  int nDevices;
+
+  cudaGetDeviceCount(&nDevices);
+  for (int i = 0; i < nDevices; i++) {
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, i);
+    printf("Device Number: %d\n", i);
+    printf("  Device name: %s\n", prop.name);
+    printf("  Memory Clock Rate (KHz): %d\n",
+           prop.memoryClockRate);
+    printf("  Memory Bus Width (bits): %d\n",
+           prop.memoryBusWidth);
+    printf("  Peak Memory Bandwidth (GB/s): %f\n",
+           2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
+    printf("  Multiprocessor counts: %d\n",prop.multiProcessorCount);
+    printf("  Warp Size: %d\n",prop.warpSize);
+    printf("  Core count: %d\n\n",getSPcores);
+  }
+}    
 
 void Labwork::labwork3_GPU() {
    
